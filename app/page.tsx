821ca4814b5ac4acef9bc633de14687da267ba93
João Vitor Dias <joaovitordias002@gmail.com>
2025-11-14 08:08:54 -0300

    feat: Adição do conteúdo base a ser exibido

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,123 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mb-10">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Commit inicial: portfolio acadêmico.
+            1. Introdução.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            João Vitor Dias Conrado Silva - Técnico em Análise e Desenvolvimento de Sistemas
+            Olá, eu sou o João Vitor Dias. Construo tecnologia com propósito.
           </p>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          Sou um desenvolvedor em formação, atualmente cursando Análise e Desenvolvimento de Sistemas na FATEC-SJC e formado como Técnico em ADS pela Etec, mas também um entusiasta da tecnologia como um todo, buscando me atualizar nas áreas de Cloud, Cybersecurity e Dados. Minha jornada na tecnologia é movida por uma filosofia simples: servir.
+          </p>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          promover o florescimento humano. Minha paixão é usar minha curiosidade na resolução de problemas e minhas habilidades técnicas e interpessoais para criar soluções que resolvam dores reais, aplicando uma mentalidade de 'apaixonar-se pelo problema e não pela solução'.
+          </p>
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Objetivo Acadêmico e Profissional
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          Meu objetivo é construir uma base sólida em Ciência da Computação para explorar as fronteiras da tecnologia, como Inteligência Artificial e (meu Grande Objetivo Audacioso) Computação Quântica. Busco me tornar um líder de tecnologia capaz de aplicar e difundir conhecimento para promover avanços científicos e educacionais no Brasil
+          </p>
+        </div>
+
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mb-10">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            2. Currículo (Formação e Conquistas).
+          </h1>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Formação Acadêmica.
+          </h2>
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+              Tecnólogo em Análise e Desenvolvimento de Sistemas (ADS) - Fatec São José dos Campos (2025 - Presente)
+            </p>
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+              Técnico em Análise e Desenvolvimento de Sistemas (ADS) - Fatec São José dos Campos (2025 - Presente)
+            </p>
+          <h2 className="max-w-xs text-2xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Principais conquistas.
+          </h2>
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+              Medalha de Bronze - 1ª Olimpíada Nacional de Inteligência Artificial (ONIA) - 2024
+            </p>
+            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Finalista da FETEPS (Feira Tecnológica do Centro Paula Souza) - Apresentação do Projeto Bluery - 2025
+            </p>
+        </div>
+
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mb-10">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            3. Principais Projetos.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">Seguindo a estrutura: Problema, Abordagem, Tecnologias, Desafios e Resultados</p>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Projeto 1: Bluery.
+          </h2>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Projeto 2: Missão Astro Júnior.
+          </h2>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Projeto 3: Vaso Irrigador Inteligente.
+          </h2>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Projeto 4: Missão Astro Júnior.
+          </h2>
+        </div>
+
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mb-10">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            4. Habilidades.
+          </h1>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Habilidades Técnicas (Hard Skills).
+          </h2>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Habilidades Interpessoais (Soft Skills)
+          </h2>
+        </div>
+
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mb-10">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            5. Documentos e Trabalhos.
+          </h1>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Trabalho de Conclusão de Curso (Técnico em ADS).
+          </h2>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            Projeto Interdisciplinar (Técnico em ADS)
+          </h2>
+        </div>
+
+
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left mb-10">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            6. Certificações e Reconhecimentos.
+          </h1>
+          <h2 className="max-w-xs text-xl leading-10 tracking-tight text-black dark:text-zinc-50">
+            [VOU CRIAR UMA GALERIA VISUAL]
+          </h2>
+          <ul>
+            <li>[Imagem/PDF] Certificado ONIA - 1ª Olimpíada Nacional de Inteligência Artificial (2024)</li>
+            <li>[Imagem/PDF] Certificado de Finalista - FETEPS 2025 (Projeto Bluery)</li>
+            <li>[Imagem/PDF] Curso de Álgebra Linear (KhanAcademy - em andamento)</li>
+            <li>[Imagem/PDF] Certificado TOEIC Bridge</li>
+            <li>[Imagem/PDF] Desafio de MicroSaaS (Rocketseat)</li>
+          </ul>
+        </div>
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            7. Contato.
+          </h1>
+          <p><a href="https://www.linkedin.com/in/joão-vitor-dias-24b0a324a">LinkedIn</a></p>
+          <p><a href="https://github.com/DiasJoao002">GitHub</a></p>
+          <p>joao.vd.conrado@gmail.com - E-mail</p>
         </div>
       </main>
     </div>
